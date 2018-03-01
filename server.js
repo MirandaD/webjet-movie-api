1ec55cd2a46479658366f4dd456873a74aaed2fa
Miranda Li <lisirui19881020@gmail.com>
2018-03-02 09:57:53 +1100

    change port

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const bodyParser = require('body-parser')
 const movieController = MovieController.create({
   needle: needle
 })
-app.use(bodyParser.json())      // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.json()) // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }))
 app.get('/movie-list', movieController.getMovieList)
 
 app.post('/movie', movieController.getDetailsByTitleAndYear)
-app.listen(3001, () => {
-  console.log('webjet-movie-api starts at 3001')
+app.listen(process.env.PORT || 3001, () => {
+  console.log('webjet-movie-api starts at ', process.env.PORT )
 })
